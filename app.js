553cf8a07c89babd86b66320e8fe66cc5ac54791
seregaMark <markovskij.ser@yandex.ru>
2024-05-27 13:42:39 +0300

    порт

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const pagesRouter = require('./routes/pages');
 // const cors = require('./middlewares/cors');
 
 const app = express();
-const PORT = 3005;
+const PORT = 3001;
 
 connectToDatabase();
 
