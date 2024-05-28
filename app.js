ec366c88f9f7300d46903cfee7725ff2c750ce0a
seregaMark <markovskij.ser@yandex.ru>
2024-05-28 23:24:37 +0300

    cors

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cookieParser = require("cookie-parser");
 const connectToDatabase = require('./database/connect');
 const apiRouter = require('./routes/apiRouter');
 const pagesRouter = require('./routes/pages');
-// const cors = require('./middlewares/cors');
+const cors = require('./middlewares/cors');
 
 const app = express();
 const PORT = 3001;
@@ -14,7 +14,7 @@ const PORT = 3001;
 connectToDatabase();
 
 app.use(
-  // cors, 
+  cors, 
   cookieParser(), 
   bodyParser.json(),
   pagesRouter,
