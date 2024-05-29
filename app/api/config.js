be1359e5d5af445be1a54fba27972150329f4e3b
seregaMark <markovskij.ser@yandex.ru>
2024-05-29 14:20:49 +0300

    url

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://pindie-backend-seregakrut.nomoredomainswork.ru/api';
+export const BASE_URL = 'https://pindie-backend-seregakrut.nomoredomainswork.ru/';
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
