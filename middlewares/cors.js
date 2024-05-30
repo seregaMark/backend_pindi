c3640ed55ef1c58afd7832e013de66ef04c10cec
seregaMark <markovskij.ser@yandex.ru>
2024-05-30 11:29:56 +0300

    финальные правки

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,7 +1,7 @@
 const allowedCors = [
   "http://localhost:3000",
   "http://localhost:3001",
-  "https://pindie-frontend-serega.nomorepartiesco.ru",
+  "https://pindie-frontend-serega.nomorepartiesco.ru", 
 ];
 
 
